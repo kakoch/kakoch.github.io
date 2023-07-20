c94ced52c3078506d166c0cb5d3371cccab529d8
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-07-19 21:42:26 -0300

    aula de hoje

---
+++ menu_ravin-main/server-itens.php
@@ -0,0 +1,32 @@
+<?php
+echo json_encode(
+   array(
+      array(
+         "id" => "1",
+         "categoria" => "bebida-alcoolida",
+         "nome" => "Choppinho Gelado",
+         "descricao" => "choppinho pilsen no caprixo!",
+         "valor" => "18.00",
+         "imagem" => "https://media.istockphoto.com/id/459396911/pt/foto/pepsi-pode-com-got%C3%ADculas-de-%C3%A1gua.jpg?s=2048x2048&w=is&k=20&c=QePAF55GUm0zkLysxl2ekn-n34iX3TDS00bGrH41pQY="
+      ),
+      array(
+         "id" => "2",
+         "categoria" => "carnes",
+         "nome" => "Angus Picanha Grelhado",
+         "descricao" => "Angus Picanha grelhado no forno a lenha feito no Viatnã",
+         "valor" => "87.00",
+         "imagem" => "https://www.shutterstock.com/image-photo/slice-hot-italian-pizza-stretching-600w-2198928269.jpg"
+      ),
+      array(
+         "id" => "3", 
+         "nome" => "Coca-Cola",
+         "categoria" => "bebidas",
+         "descricao" => "Coca-Cola sabor original é uma bebida carbonatada feita a partir de água gaseificada, açúcar, extrato de noz de cola, cafeína, corante caramelo IV, acidulante ácido fosfórico e aroma natural. Cada 200 ml contém aproximadamente 85 kcal e 10 mg de sódio. É uma das bebidas mais populares e consumidas em todo o mundo, conhecida por seu sabor único e refrescante.", 
+         "valor" => "3,49", 
+         "unidade" => "ml", 
+         "quantidade" => "350", 
+         "imagem" => "https://www.shutterstock.com/image-photo/ankara-turkey-may-17-2014-600w-193222430.jpg"
+               
+      )
+   )
+);
